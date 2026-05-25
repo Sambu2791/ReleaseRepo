49319db2464f474de6d64c82eb9b272195479074
Sambu <amsam27@gmail.com>
2026-05-25 17:00:21 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ReleaseRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Andres Samboni",                         # Tu nombre
+    author_email="amsam27@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Sambu2791/ReleaseRepo",     # URL del proyecto
+)
